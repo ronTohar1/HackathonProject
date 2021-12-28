6dad7075ec86ff82108c710d2e97cf4c5f1d6684
tomerhal <tomerhal@post.bgu.ac.il>
2021-12-28 15:07:38 +0200

    client game mode with select

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 from struct import unpack
+import sys
+import select
 
 BROADCAST_PORT =13117
 MAGIC_COOKIE = 0xabcddcba
 OFFER_MSG_TYPE = 0x2
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 20001)
-bufferSize          = 1024
 
 
 def start_client():
@@ -15,43 +15,70 @@ def start_client():
     ClientBroadcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ClientBroadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     ClientBroadcastSocket.bind(("", BROADCAST_PORT))
-    self.recive_broadcast(ClientBroadcastSocket)
+    recive_broadcast(ClientBroadcastSocket)
 
 def recive_broadcast(socket):
     packet, address= socket.recvfrom(1024)
     cookie = packet[:4]
     msg_type = packet[4:5]
-    if cookie == MAGIC_COOKIE and msg_type ==:
+    if cookie == MAGIC_COOKIE and msg_type ==OFFER_MSG_TYPE:
         server_port = unpack('h', packet[5:7])
-        connect_to_server(server_port)
+        connect_to_server(server_port,address)
     recive_broadcast(socket)
 
-def connect_to_server(server_port):
+def connect_to_server(server_port, server_address):
     ClientConnectionSocket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        ClientConnectionSocket.connect((HOST, server_port))
+        ClientConnectionSocket.connect((server_address, server_port))
     except ConnectionRefusedError:
         ClientConnectionSocket.close()
         recive_broadcast()
-    game_mode(connection_socket)
+    game_mode(ClientConnectionSocket)
 
 def game_mode(connection_socket):
-    reciver = threading.Thread(target=print_msg, args=(ClientConnectionSocket))
-    sender = threading.Thread(target=send_keys, args=(ClientConnectionSocket))
-    reciver.start()
-    sender.start()
-    reciver.join()
+    msg_queue = []
+    should_finish = False
+    while not should_finish:
+        readable,writable,e = select.select([sys.stdin,connection_socket],[],[],0.0001)
+        for s in readable:
+            if s == sys.stdin:
+                msg_queue.extend(handle_keyboard(s))
+                writable.append(connection_socket)
+            if s == connection_socket:
+                if handle_msg_from_socket(s):
+                    should_finish = True 
+        for o in writable:
+            handle_send(o,msg_queue.pop())
+            if(len(msg_queue)==0):
+                writable.remove(o)
+        for s in e:
+            print >>sys.stderr, 'handling exceptional condition for', s.getpeername()
+            # Stop listening for input on the connection
+            readable.remove(s)
+            if s in writable:
+                writable.remove(s)
+            s.close()
+            should_finish = True
+    
+def handle_msg_from_socket(s):
+    data = s.recv(1024)
+    if data:
+        # A readable client socket has data
+        print >>sys.stderr, 'received "%s" from %s' % (data, s.getpeername())
+        return False #should not finish
+    else:
+            # Interpret empty result as closed connection
+            print >>sys.stderr, 'closing', s.getpeername(), 'after reading no data'
+            # Should finish
+            return True
+                
+def handle_keyboard(s):
+    # gets a msg from server using created TCP socket
+    input = s.readline()
+    chars = []
+    for c in input:
+        chars.append(c)
+    return chars
 
-def recive_q(connection_socket):
-    data = connection_socket.recv(1024)
-    print(data)
-    data = connection_socket.recv(1024)
-    p
-
-# Send to server using created UDP socket
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
+def handle_send(o,msg):
+    o.send(msg)
